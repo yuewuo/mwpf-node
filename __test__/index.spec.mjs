6c1ec230570d967fed8a2c7ecc97ec911e3f6076
Yue Wu <yue.wu@yale.edu>
2025-06-19 16:36:05 -0400

    finish example

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,21 +1,168 @@
 import test from 'ava'
-
 import { solve } from '../index.js'
+import fs from 'fs'
 
 test('print initializer', async (t) => {
+  const code = new RotatedSurfaceCode(5)
   try {
-    let result = await solve({
-      initializer: {
-        vertex_num: 5,
-        weighted_edges: [],
-        heralds: [],
+    let result_str = await solve({
+      initializer: code.initializer(),
+      positions: code.visualize_positions(),
+      syndrome_pattern: {
+        defect_vertices: [7, 8, 13],
       },
-      // with_json: true,
-      // with_html: true,
+      with_json: true,
+      with_html: true,  // this is more expensive, only use for debugging
     })
+    let result = JSON.parse(result_str)
+    const json = result.json  // contains the visualizer data
+    const html = result.html  // can be saved to a file and opened in a browser
+    console.log(json.snapshots)  // this is the step-by-step decoding process
+    if (html) {
+      const filePath = 'index.spec.mjs.html';
+      fs.writeFileSync(filePath, html)
+      console.log(`HTML saved to ${filePath}`)
+    }
+    t.pass()
   } catch (e) {
     console.error("An error occurred:", e.message);
+    t.fail()
   }
-  // t.is(sum(1, 2), 3)
-  t.pass()
 })
+
+
+export class RotatedSurfaceCode {
+  constructor(d, with_x_error = true, with_z_error = true, with_y_error = true) {
+    this.d = d
+    this.with_x_error = with_x_error
+    this.with_z_error = with_z_error
+    this.with_y_error = with_y_error
+    // add vertices of the decoding graph
+    this.positions = []
+    this.position_to_vertex_idx = new Map()
+    for (let i = -1; i <= 2 * this.d - 1; i++) {
+      for (let j = -1; j <= 2 * this.d - 1; j++) {
+        if (this.is_stabilizer(i, j)) {
+          const position = { i, j }
+          this.position_to_vertex_idx[`${i},${j}`] = this.positions.length
+          this.positions.push(position)
+        }
+      }
+    }
+    this.data_qubit_positions = []
+    this.position_to_data_qubit_idx = {}
+    for (let i = 0; i < 2 * this.d - 1; i++) {
+      for (let j = 0; j < 2 * this.d - 1; j++) {
+        if (this.is_data_qubit(i, j)) {
+          const position = { i, j }
+          this.position_to_data_qubit_idx[position] = this.data_qubit_positions.length
+          this.data_qubit_positions.push(position)
+        }
+      }
+    }
+    this.weighted_edges = []
+    this.edge_error_info = []
+    for (const data_qubit_position of this.data_qubit_positions) {
+      const i = data_qubit_position.i
+      const j = data_qubit_position.j
+      const get_syndrome = (i, j, type) => {
+        const syndrome = []
+        for (const [di, dj] of [[1, 1], [1, -1], [-1, -1], [-1, 1]]) {
+          if (this.is_x_stabilizer(i + di, j + dj) && type != 'X') {
+            syndrome.push(this.position_to_vertex_idx[`${i + di},${j + dj}`])
+          } else if (this.is_z_stabilizer(i + di, j + dj) && type != 'Z') {
+            syndrome.push(this.position_to_vertex_idx[`${i + di},${j + dj}`])
+          }
+        }
+        return syndrome
+      }
+      // Pauli X error
+      for (const [enable, type] of [[this.with_x_error, 'X'], [this.with_z_error, 'Z'], [this.with_y_error, 'Y']]) {
+        if (enable) {
+          const syndrome = get_syndrome(i, j, type)
+          this.edge_error_info.push({
+            syndrome,
+            error_type: type,
+            data_qubit_position,
+          })
+          this.weighted_edges.push({ vertices: syndrome })
+        }
+      }
+    }
+  }
+
+  is_qubit(x, y) {
+    // the top-left data qubit is (0, 0), the top-right data qubit is (0, 2d-1)
+    if (x < -1 || x > 2 * this.d - 1 || y < -1 || y > 2 * this.d - 1) {
+      return false
+    }
+    if (x == -1 || x == 2 * this.d - 1) {
+      if (y <= -1 || y >= 2 * this.d - 1) {
+        return false
+      }
+      if (x == -1) {
+        return y % 4 == 3
+      } else {
+        return y % 4 == 1
+      }
+    }
+    if (y == -1) {
+      return x % 4 == 1
+    }
+    if (y == 2 * this.d - 1) {
+      return x % 4 == 3
+    }
+    return (x + y) % 2 == 0
+  }
+
+  is_data_qubit(x, y) {
+    return this.is_qubit(x, y) && x % 2 == 0 && y % 2 == 0
+  }
+
+  is_stabilizer(x, y) {
+    return this.is_qubit(x, y) && !this.is_data_qubit(x, y)
+  }
+
+  is_x_stabilizer(x, y) { // green
+    return this.is_stabilizer(x, y) && (x + y) % 4 == 0
+  }
+
+  is_z_stabilizer(x, y) { // red
+    return this.is_stabilizer(x, y) && (x + y) % 4 == 2
+  }
+
+  initializer() {
+    return {
+      vertex_num: this.positions.length,
+      weighted_edges: this.weighted_edges,
+    }
+  }
+
+  visualize_positions() {
+    return this.positions.map(position => ({
+      i: position.i / 1.5,
+      j: position.j / 1.5,
+    }))
+  }
+}
+
+export function parse_rust_bigint(data) { // (data: any): bigint | number {
+  if (typeof data === 'number' || typeof data === 'bigint') {
+    return data
+  } else if (typeof data === 'string') {
+    return BigInt(data)
+  } else if (typeof data === 'object') {
+    assert(data.length === 2)
+    const [sign, digits] = data
+    assert(typeof sign === 'number')
+    assert(sign == 1 || sign == -1 || sign == 0)
+    assert(typeof digits === 'object')
+    let value = 0n
+    for (let i = digits.length - 1; i >= 0; i--) {
+      value = (value << 32n) + BigInt(digits[i])
+    }
+    return BigInt(sign) * value
+  } else {
+    throw new Error(`invalid data type: ${typeof data}`)
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,34 @@
 #[macro_use]
 extern crate napi_derive;
 
+use mwpf::mwpf_solver::{SolverSerialJointSingleHair, SolverTrait};
 use mwpf::util::*;
+use mwpf::visualize::*;
 use napi::bindgen_prelude::*;
-use napi::JsUnknown;
+use napi::JsString;
+use num_traits::cast::ToPrimitive;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::sync::Arc;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolverInput {
   initializer: SolverInitializer,
+  syndrome_pattern: SyndromePattern,
+  #[serde(default = "Default::default")]
+  positions: Vec<VisualizePosition>,
+  #[serde(default = "empty_config")]
+  solver_config: serde_json::Value,
   #[serde(default = "Default::default")]
   with_json: bool,
   #[serde(default = "Default::default")]
   with_html: bool,
+  #[serde(default = "empty_config")]
+  html_override_config: serde_json::Value,
+}
+
+fn empty_config() -> serde_json::Value {
+  json!({})
 }
 
 pub struct AsyncSolve {
@@ -24,13 +39,56 @@ pub struct AsyncSolve {
 
 impl Task for AsyncSolve {
   type Output = serde_json::Value;
-  type JsValue = JsUnknown;
+  type JsValue = JsString; // we cannot use JsObject here because otherwise the bigint will be converted to something like {"$serde_json::private::Number": "1.0"}
 
   fn compute(&mut self) -> Result<Self::Output> {
     let result = std::panic::catch_unwind(|| -> serde_json::Value {
       let problem: SolverInput = serde_json::from_value(self.input.clone()).unwrap();
-      println!("problem: {:?}", problem);
-      json!({})
+      let mut visualizer = None;
+      if problem.with_json || problem.with_html {
+        visualizer =
+          Some(Visualizer::new(Some(String::new()), problem.positions.clone(), true).unwrap());
+      }
+      let mut solver = SolverSerialJointSingleHair::new(
+        &Arc::new(problem.initializer.clone()),
+        problem.solver_config.clone(),
+      );
+      solver.solve_visualizer(problem.syndrome_pattern.clone(), visualizer.as_mut());
+      let (subgraph, weight_range) = solver.subgraph_range_visualizer(visualizer.as_mut());
+      // construct result
+      let mut result = serde_json::Map::new();
+      if problem.with_json {
+        result.insert(
+          "json".to_string(),
+          visualizer.as_mut().unwrap().get_visualizer_data(),
+        );
+      }
+      if problem.with_html {
+        result.insert(
+          "html".to_string(),
+          visualizer
+            .as_mut()
+            .unwrap()
+            .generate_html(problem.html_override_config.clone())
+            .into(),
+        );
+      }
+      result.insert(
+        "subgraph".to_string(),
+        subgraph.iter().cloned().collect::<Vec<_>>().into(),
+      );
+      result.insert(
+        "weight_range".to_string(),
+        json!({
+          "lower": weight_range.lower.to_f64(),
+          "upper": weight_range.upper.to_f64(),
+          "ln": numer_of(&weight_range.lower),
+          "ld": denom_of(&weight_range.lower),
+          "un": numer_of(&weight_range.upper),
+          "ud": denom_of(&weight_range.upper),
+        }),
+      );
+      result.into()
     });
     match result {
       Ok(result) => Ok(result),
@@ -39,7 +97,10 @@ impl Task for AsyncSolve {
   }
 
   fn resolve(&mut self, env: Env, output: Self::Output) -> Result<Self::JsValue> {
-    env.to_js_value(&output)
+    let result_str = serde_json::to_string(&output).unwrap();
+    env
+      .to_js_value(&result_str)
+      .map(|v| JsString::try_from(v))?
   }
 
   fn reject(&mut self, _env: Env, err: Error) -> Result<Self::JsValue> {
